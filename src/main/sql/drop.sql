e6784d772a605332f5601b352bf0cf90596da229
RooiGevaar19 <redthreat19@gmail.com>
2017-10-24 17:53:56 +0200

    Dodanie testowych skryptów SQL

---
+++ src/main/sql/drop.sql
@@ -0,0 +1,6 @@
+DROP TABLE Snapshot;
+DROP TABLE TextFile;
+DROP TABLE Attendance;
+DROP TABLE Workgroup;
+DROP TABLE Account;
+DROP TABLE Role;
---
+++ src/main/sql/insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO Role (RoleName) VALUES ('Administrator');
+INSERT INTO Role (RoleName) VALUES ('Moderator');
+INSERT INTO Role (RoleName) VALUES ('User');
