c19059582bbd84069031efa340c8f3d21c58fab4
mzakrzewski <michzak30@gmail.com>
2017-12-05 12:29:17 +0100

    Servlety Usuwania i Edycji Grupa/Plik

---
+++ src/main/java/actions/TextFileUpdater.java
@@ -0,0 +1,37 @@
+package actions;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.TextFileManager;
+
+@WebServlet("/doEditTextFile")
+public class TextFileUpdater extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		TextFileManager db = new TextFileManager();
+		try {
+			int id = Integer.parseInt(request.getParameter("id"));
+			String name = request.getParameter("name");
+			String description = request.getParameter("description");
+			boolean isPrivate = Boolean.parseBoolean(request.getParameter("private"));
+			db.doTranBegin();
+			db.editTextFileByParams(id, name, description,isPrivate);
+			db.doTranCommit();
+			response.sendRedirect("/subpages/updateSuccess.jsp");
+		} catch (SQLException e) {
+			db.doTranRollback();
+			e.printStackTrace();
+			response.sendRedirect("/subpages/updateFail.jsp");
+		} finally {
+			db.doTranEnd();
+		}
+	}
+}
